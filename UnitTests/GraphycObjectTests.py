1e31ddf108ab83689448962cbe0b718a5acb5249
Federico Orlandini <federico.orlandini@gmail.com>
2016-09-09 00:35:49 +0200

    Some code clean up

--- UnitTests/GraphycObjectTests.py
+++ UnitTests/GraphycObjectTests.py
@@ -10,32 +10,35 @@ from Main.graphicobjects import StarShip
 from Main.geometrytransformation2d import Vector2D
 from Main.engines import World
 
-#-----------------------------------------------------------------------
+
+# -----------------------------------------------------------------------
 class GraphicObjectTests(unittest.TestCase):
     def test_move_shouldChangeThePositionOfTheObjectInTheCorrectWay(self):
-        obj = GraphicObject()
+        # fake world
+        world = World((100, 100))
+        obj = GraphicObject(world)
         movement_angle = 45
         movement_length = 10
-        
+
         expected_x = 10 / math.sqrt(2)
         expected_y = expected_x
-        
+
         obj._move(movement_angle, movement_length)
-        
+
         self.assertTrue(values.are_equals(obj.position.x, expected_x), "The X coordinate is wrong")
         self.assertTrue(values.are_equals(obj.position.y, expected_y), "The y coordinate is wrong")
 
-
     def test_rotate_shoudUpdateTheHeadAngleOfTheObject(self):
-        obj = GraphicObject(vertexes_local= (Vector2D(1, 0),))
+        # fake world
+        world = World((100, 100))
+        obj = GraphicObject(world, vertexes_local=(Vector2D(1, 0),))
         rotation_angle = 45
-        
+
         obj.rotate_head_direction(rotation_angle)
 
         self.assertEqual(obj.head_angle, rotation_angle)
 
-    @unittest.mock.patch('pygame.Surface')
-    def test_get_collision_circle(self, mockSurface):
+    def test_get_collision_circle(self):
         # fake world
         world = World((100, 100))
         # Prepare the object with vertexes like a square with the center in (0, 0) and
@@ -49,8 +52,7 @@ class GraphicObjectTests(unittest.TestCase):
         self.assertTrue(values.are_equals(graph_object.collision_circle.radius, expected_radius))
 
     @unittest.mock.patch('Main.graphicobjects.GraphicObject._compute_collision_circle')
-    @unittest.mock.patch('pygame.Surface')
-    def test_process_should_update_the_collision_circle(self, mocksurface, mock):
+    def test_process_should_update_the_collision_circle(self, mock):
         # fake world
         world = World((100, 100))
         # Prepare the object with vertexes like a square with the center in (0, 0) and
@@ -60,33 +62,33 @@ class GraphicObjectTests(unittest.TestCase):
         graph_object.process(1)
         self.assertTrue(mock.called)
 
-#-----------------------------------------------------------------------
+
+# -----------------------------------------------------------------------
 class StarShipTests(unittest.TestCase):
+    # fake world
+    _world = World((100, 100))
     _lookup_table = lookuptables.CosSinTable()
 
-
     def test_fire_withReloadCounterNotZero_shouldNotFireABullet(self):
-        ship = StarShip(0, 0, constants.WHITE, self._lookup_table)
+        ship = StarShip(self._world, 0, 0, constants.WHITE, self._lookup_table)
         ship.reload_counter = 1
         bullet = ship.fire()
         self.assertIsNone(bullet)
 
-
     def test_fire_withReloadCounterZero_shouldFireABullet(self):
-        ship = StarShip(0, 0, constants.WHITE, self._lookup_table)
+        ship = StarShip(self._world, 0, 0, constants.WHITE, self._lookup_table)
         ship.reload_counter = 0
         bullet = ship.fire()
         self.assertIsNotNone(bullet)
 
-
     def test_fire_withReloadCounterZero_shouldResetTheReloadCounter(self):
-        ship = StarShip(0, 0, constants.WHITE, self._lookup_table)
+        ship = StarShip(self._world, 0, 0, constants.WHITE, self._lookup_table)
         ship.reload_counter = 0
-        bullet = ship.fire()
+        ship.fire()
         self.assertNotEqual(ship.reload_counter, 0)
 
 
-#-----------------------------------------------------------------------
+# -----------------------------------------------------------------------
 if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'Test.testName']
+    # import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
